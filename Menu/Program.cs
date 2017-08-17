a14fd77291ca3ea81125d47b5eb867aa5179e2a9
JamesBabz <web.fuhlendorff@gmail.com>
2017-08-17 11:37:49 +0200

    gui
    gui

--- Menu/Program.cs
+++ Menu/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Security;
+using static System.Console;
 
 namespace Menu
 {
@@ -6,7 +8,57 @@ namespace Menu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] menuItems =
+            {
+                "Show Videos",
+                "Add Video",
+                "Delete Video",
+                "Edit Video",
+                "Exit"
+            };
+
+            var selection = ShowMenu(menuItems);
+
+            while (selection != 5)
+            {
+                switch (selection)
+                {
+                    case 1:
+                        WriteLine("Show Videos");
+                        break;
+                    case 2:
+                        WriteLine("Add Video");
+                        break;
+                    case 3:
+                        WriteLine("Delete Video");
+                        break;
+                    case 4:
+                        WriteLine("Edit Videos");
+                        break;
+                    default:
+                        WriteLine("Mojn");
+                        break;
+                }
+                selection = ShowMenu(menuItems);
+            }
+            ReadLine();
+        }
+
+        private static int ShowMenu(string[] menuItems)
+        {
+            Clear();
+            WriteLine("Select what to do:\n");
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                WriteLine($"{i + 1}. {menuItems[i]}");
+            }
+            int selection;
+            while (!int.TryParse(ReadLine(), out selection)
+                   || selection < 1 || selection > menuItems.Length)
+            {
+                WriteLine("That is not a valid input");
+            }
+            return selection;
         }
     }
 }
